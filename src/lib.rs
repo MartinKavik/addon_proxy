67b22bda59ed3fd59c378e9af006f56ba52a16a5
Martin Kavík <martin@kavik.cz>
2020-04-08 16:55:50 +0200

    handle_routes

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 
 use hyper::{Body, Client, Request, Response};
 use hyper::client::HttpConnector;
+use http::StatusCode;
 
 pub mod proxy;
 use proxy::{ProxyConfig, ScheduleConfigReload};
@@ -13,7 +14,7 @@ pub async fn on_request(
     proxy_config: Arc<ProxyConfig>,
     schedule_config_reload: ScheduleConfigReload,
 ) -> Result<Response<Body>, hyper::Error> {
-    println!("proxy config: {:#?}", proxy_config);
+    // println!("proxy config: {:#?}", proxy_config);
     println!("original req: {:#?}", req);
 
     let req = try_map_request(req, &proxy_config, schedule_config_reload);
@@ -41,9 +42,33 @@ fn handle_config_reload(req: Request<Body>, proxy_config: &ProxyConfig, schedule
     Ok(req)
 }
 
-/// Update request's URI to point to another server according to predefined routes.
+/// Update request's URI to point to another address according to predefined routes.
+/// Returns BAD_REQUEST response if there is no matching route.
 fn handle_routes(mut req: Request<Body>, proxy_config: &ProxyConfig) -> Result<Request<Body>, Response<Body>> {
-    *req.uri_mut() = "http://localhost:8000/".parse().unwrap();
+    let uri = req.uri_mut();
+    // http://example.com/abc/efg?x=1&y=2 -> example.com/abc/efg?x=1&y=2
+    let from = format!("{}{}{}", uri.host().unwrap_or_default(), uri.path(), uri.query().unwrap_or_default());
+
+    // Get the first matching route or return BAD_REQUEST.
+    let route = proxy_config.routes.iter().find(|route| {
+        from.starts_with(&route.from)
+    });
+    let route = match route {
+        Some(route) => route,
+        None => {
+            let mut response = Response::new(Body::from("No route matches."));
+            *response.status_mut() = StatusCode::BAD_REQUEST;
+            return Err(response)
+        }
+    };
+
+    // @TODO: Replace `trim_start_matches` with `strip_prefix` once stable.
+    // example.com/abc/efg?x=1&y=2 -> abc/efg?x=1&y=2  (if matching route's `from` is "example.com")
+    let routed_path_and_query = from.trim_start_matches(&route.from).trim_start_matches("/");
+    // abc/efg?x=1&y=2 -> http://localhost:8000/abc/efgx=1&y=2 (if matching route's `to` is "http://localhost:8000")
+    let new_uri = format!("{}{}", route.to, routed_path_and_query);
+
+    *uri = new_uri.parse().expect("routed uri");
     Ok(req)
 }
 
