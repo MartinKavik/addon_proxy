235255d418adc6ed2c98399ead25887b819b41d9
Martin Kavík <martin@kavik.cz>
2020-06-19 17:26:38 +0200

    caching.rs tests + updated docs

--- src/helpers.rs
+++ src/helpers.rs
@@ -10,6 +10,11 @@ pub fn set_now_getter(now: impl Fn() -> i64 + Send + Sync + 'static) {
     *NOW_GETTER.write().unwrap() = Arc::new(now);
 }
 
+/// Get the current timestamp. 
+///
+/// It should be used instead of `chrono::Utc::now()` to allow time manipulation in tests.
+///
+/// There shouldn't be any noticeable performance penalties according to the benchmarks.
 pub fn now_timestamp() -> i64 {
     NOW_GETTER.read().unwrap()()
 }
--- tests/caching.rs
+++ tests/caching.rs
@@ -39,64 +39,114 @@ mod caching {
 
 
     #[tokio::test]
-    async fn cache() {
-        clear_cache().await;
+    async fn caching_test_suite() {
         let client = Client::new();
-
         // Run tests sequentially because we need to manipulate with time and modify cache. 
-
         test_no_headers(&client).await;
+        test_max_age_header(&client).await;
+        test_stale_response(&client).await;
     }
 
-    // if no cache headers are returned at all, assume 10 minutes cache validity
+    // "If no cache headers are returned at all, assume 10 minutes cache validity."
     async fn test_no_headers(client: &Client<HttpConnector>) {
         // ------ ARRANGE ------
+        clear_cache().await;
+        set_now_getter(|| Utc::now().timestamp());
 
         let mock_server = start_mock_server();
         let resource = mock_server.create_resource("/catalog/movie/top.json");
         resource
-            .header("Content-Type", "application/json")
             .body(include_str!("../test_data/top.json"));
 
         let path = "/origin/catalog/movie/top.json";
+        let send_request = || async { client.get(url_from_path(path)).await.unwrap() };
 
         // ------ ACT ------
 
-        let res = client.get(url_from_path(path)).await.unwrap();
-        assert_eq!(res.status(), StatusCode::OK,);
+        send_request().await;  // This request should be loaded from the origin.
+        send_request().await;
 
-        // this request should be loaded from cache
-        let res = client.get(url_from_path(path)).await.unwrap();
-        assert_eq!(res.status(), StatusCode::OK,);
-
-        // move time (> default_cache_validity)       
+        // Jump in time: +11 min. Jump has to be longer than `default_cache_validity` in proxy config.       
         set_now_getter(|| Utc::now().timestamp() + (11 * 60)); 
 
-        let res = client.get(url_from_path(path)).await.unwrap();
-        assert_eq!(res.status(), StatusCode::OK,);
-
-        // this request should be loaded from cache
-        let res = client.get(url_from_path(path)).await.unwrap();
-        assert_eq!(res.status(), StatusCode::OK,);
+        send_request().await; // This request should be loaded from the origin.
+        send_request().await;
+        send_request().await;
 
         // ------ ASSERT ------
         
+        // 3 requests should be loaded from cache, 2 from the origin.
         assert_eq!(resource.request_count(), 2);
     }
 
+    // "No need to handle all HTTP cache header specs: just `cache-control` `max-age`"
+    async fn test_max_age_header(client: &Client<HttpConnector>) {
+        // ------ ARRANGE ------
+        clear_cache().await;
+        set_now_getter(|| Utc::now().timestamp());
 
+        let mock_server = start_mock_server();
+        let resource = mock_server.create_resource("/catalog/movie/top.json");
+        resource
+            // 300s = 5 min
+            .header("Cache-Control", "max-age=300")
+            .body(include_str!("../test_data/top.json"));
 
-    // no need to handle all HTTP cache header specs: just `cache-control` `max-age`
+        let path = "/origin/catalog/movie/top.json";
+        let send_request = || async { client.get(url_from_path(path)).await.unwrap() };
+
+        // ------ ACT ------
+
+        send_request().await;  // This request should be loaded from the origin.
+        send_request().await;
+
+        // Jump in time: +6 min. Jump has to be longer than `max-age` in the request header.       
+        set_now_getter(|| Utc::now().timestamp() + (6 * 60)); 
+
+        send_request().await; // This request should be loaded from the origin.
+        send_request().await;
+        send_request().await;
+
+        // ------ ASSERT ------
+        
+        // 3 requests should be loaded from cache, 2 from the origin.
+        assert_eq!(resource.request_count(), 2);
+    }
 
     // if the addon origin is failing for some reason (returning non-200, timing out), return the last cached response, even if it's stale*
     // * - there should be a configurable "stale threshold": e.g., only do this if the cached response is not older than 48 hours
+    async fn test_stale_response(client: &Client<HttpConnector>) {
+        // ------ ARRANGE ------
+        clear_cache().await;
+        set_now_getter(|| Utc::now().timestamp());
 
-    // #[tokio::test]
-    // async fn top() {
-    //     let path = "/origin/catalog/movie/top.json";
-    //     let res = Client::new().get(url_from_path(path)).await.unwrap();
-    //     assert_eq!(res.status(), StatusCode::OK,);
-    // }
+        let mock_server = start_mock_server();
+        let resource = mock_server.create_resource("/catalog/movie/top.json");
+        resource
+            .body(include_str!("../test_data/top.json"));
+
+        let path = "/origin/catalog/movie/top.json";
+        let send_request = || async { client.get(url_from_path(path)).await.unwrap() };
+
+        // ------ ACT ------
+
+        assert_eq!(send_request().await.status(), StatusCode::OK);  // This request should be loaded from the origin.
+        assert_eq!(send_request().await.status(), StatusCode::OK);
+
+        // Kill `mock_server` to simulate addon fail.
+        drop(mock_server);
+
+        assert_eq!(send_request().await.status(), StatusCode::OK);
+        assert_eq!(send_request().await.status(), StatusCode::OK);
+
+        // Jump in time: +49 h. Jump has to be longer than `cache_stale_threshold_on_fail` in proxy config.      
+        set_now_getter(|| Utc::now().timestamp() + (49 * 60 * 60)); 
+
+        // ------ ASSERT ------
+        
+        assert_eq!(send_request().await.status(), StatusCode::INTERNAL_SERVER_ERROR);
+        assert_eq!(send_request().await.status(), StatusCode::INTERNAL_SERVER_ERROR);
+    }
 
     // ------ SETUP HELPERS ------
 
